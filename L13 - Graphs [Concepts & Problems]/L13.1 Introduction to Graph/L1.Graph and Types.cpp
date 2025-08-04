972baba1e0100f190dc6141f8aa00c9e86694c99
Sane <saneraza78692@gmail.com>
2025-08-04 22:27:40 +0530

    Add : 3 Lectures completed Graphs

---
+++ L13 - Graphs [Concepts & Problems]/L13.1 Introduction to Graph/L1.Graph and Types.cpp
@@ -0,0 +1,8 @@
+// What is a graph data structure?
+// There are two types of data structures
+
+// Linear
+// Non - linear
+// We are aware of linear data structures such as arrays, stacks, queues, and linked lists. They are called linear because data elements are arranged in a linear or sequential manner.
+
+// The only non-linear data structure that we’ve seen so far is Tree. In fact, a tree is a special type of graph with some restrictions. Graphs are data structures that have a wide-ranging application in real life. These include analysis of electrical circuits, finding the shortest routes between two places, building navigation systems like Google Maps, even social media using graphs to store data about each user, etc. To understand and use the graph data structure, let’s get familiar with the definitions and terms associated with graphs.
